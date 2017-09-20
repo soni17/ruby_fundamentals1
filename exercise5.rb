c2bcfebed7c3f697a720161cb8d8c2fd18eb5454
soni17 <elb004@gmail.com>
2017-09-20 11:17:55 -0400

    removed spaces to make it more readable

--- exercise5.rb
+++ exercise5.rb
@@ -8,29 +8,19 @@ while distance < 50
   entry.to_s
 
   if entry == "walk"
-
     energy = energy + 3
     distance = distance + 1
     puts "Distance from home is #{distance}km and your energy is #{energy}"
-
   elsif entry == "run" && energy >= 4
-
     energy = energy - 4
     distance = distance + 5
     puts "Distance from home is #{distance}km and your energy is #{energy}"
-
   elsif entry == "run" && energy < 4
-
     puts "you don't have enough energy to run. Your energy is #{energy} and you need at least 4."
-
   elsif entry == "go home"
-
     distance = 51
-
   else
-
     puts "command not recognized"
-
   end
 
 end
