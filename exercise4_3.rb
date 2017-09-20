31156a16368cfb8aeba605d65df3cb73ccc5f4b5
soni17 <elb004@gmail.com>
2017-09-20 00:47:40 -0400

    exercise 4-3 completed

---
+++ exercise4_3.rb
@@ -0,0 +1,10 @@
+my_name = "Jimmy"
+puts "What is your name ?"
+name_entered = gets.chomp
+
+if my_name === name_entered
+  puts "we have the same name"
+else 
+  puts "we have different names"
+end
+ 
